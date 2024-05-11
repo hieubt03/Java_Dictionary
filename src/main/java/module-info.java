1283b47f1a20d856219953c25d1235efa31c1e02
vutuanha102 <84643216+vutuanha102@users.noreply.github.com>
2024-05-12 01:49:50 +0700

    Add google api

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,14 @@ module org.example.java_dictionary {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires freetts;
+
     requires google.cloud.translate;
-    //requires proto.google.cloud.translate.v3;
+//    requires proto.google.cloud.translate.v3;
+
+    requires google.cloud.core;
+    requires google.cloud.firestore;
+    requires com.google.auth;
+    requires com.google.auth.oauth2;
 
     opens org.java_dictionary to javafx.fxml;
     exports org.java_dictionary;
--- src/main/java/org/java_dictionary/GoogleAPIController.java
+++ src/main/java/org/java_dictionary/GoogleAPIController.java
@@ -1,17 +1,16 @@
 package org.java_dictionary;
 
 import DictionaryCommandLine.DictionaryManagement;
-//import com.google.cloud.translate.v3.LocationName;
-//import com.google.cloud.translate.v3.TranslateTextRequest;
-//import com.google.cloud.translate.v3.TranslateTextResponse;
-//import com.google.cloud.translate.v3.Translation;
-//import com.google.cloud.translate.v3.TranslationServiceClient;
+
 import java.io.IOException;
+
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+
+import com.google.cloud.translate.*;
 
 public class GoogleAPIController {
     private DictionaryManagement dic;
@@ -31,44 +30,17 @@ public class GoogleAPIController {
     void OnTranslateButtonClicked(MouseEvent event) {
         String text = SourceText.getText();
 
+        Translate translate = TranslateOptions.newBuilder().setApiKey(API_KEY).build().getService();
 
-
-
-        TranslatedText.setText(text);
-
-    }
- /*
-    public static void translateText() throws IOException {
-        String projectId = "charged-gravity-422909-f7";
-        String targetLanguage = "es";
-        String text = "Hello world!";
-        translateText(projectId, targetLanguage, text);
-    }
-
-    public static void translateText(String projectId, String targetLanguage, String text)
-            throws IOException {
-
-        try (TranslationServiceClient client = TranslationServiceClient.create()) {
-            LocationName parent = LocationName.of(projectId, "global");
-
-            TranslateTextRequest request =
-                    TranslateTextRequest.newBuilder()
-                            .setParent(parent.toString())
-                            .setMimeType("text/plain")
-                            .setTargetLanguageCode(targetLanguage)
-                            .addContents(text)
-                            .build();
-
-            TranslateTextResponse response = client.translateText(request);
-
-            // Display the translation for each input text provided
-            for (Translation translation : response.getTranslationsList()) {
-                System.out.printf("Translated text: %s\n", translation.getTranslatedText());
-            }
-        }
+        Translation translation = translate.translate(text,Translate.TranslateOption.targetLanguage("vi"));
+        TranslatedText.setText(translation.getTranslatedText());
 
     }
-*/
+
+
+
+
+
 
 
 }
