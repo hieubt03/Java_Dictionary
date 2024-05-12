5785249cca6a6b7aba0b92988bb3a31c03d7534f
vutuanha102 <84643216+vutuanha102@users.noreply.github.com>
2024-05-12 17:56:04 +0700

    Add change target language

--- src/main/java/org/java_dictionary/GoogleAPIController.java
+++ src/main/java/org/java_dictionary/GoogleAPIController.java
@@ -3,10 +3,12 @@ package org.java_dictionary;
 import DictionaryCommandLine.DictionaryManagement;
 
 import java.io.IOException;
+import java.lang.annotation.Target;
 
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
@@ -20,7 +22,13 @@ public class GoogleAPIController {
     private TextField SourceText;
     @FXML
     private TextField TranslatedText;
+    @FXML
+    private Button ChangeTargetLangButton;
+    @FXML
+    private Label TargetLangText;
     private static final String API_KEY = "AIzaSyCARQj8ecJRRuQ5DPng2ULE89ej1DhOp9g";
+
+    private String TargetLang = "vi";
 
     void initData(DictionaryManagement dic) {
         this.dic = dic;
@@ -32,9 +40,20 @@ public class GoogleAPIController {
 
         Translate translate = TranslateOptions.newBuilder().setApiKey(API_KEY).build().getService();
 
-        Translation translation = translate.translate(text,Translate.TranslateOption.targetLanguage("vi"));
+        Translation translation = translate.translate(text,Translate.TranslateOption.targetLanguage(TargetLang));
         TranslatedText.setText(translation.getTranslatedText());
 
+    }
+
+    @FXML
+    void OnChangeTargetLang(MouseEvent event) {
+        if (TargetLang == "vi"){
+            TargetLang = "en";
+            TargetLangText.setText("Việt sang Anh");
+        } else {
+            TargetLang = "vi";
+            TargetLangText.setText("Anh sang Việt");
+        }
     }
 
 
